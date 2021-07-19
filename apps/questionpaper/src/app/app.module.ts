5b26654a19e7b3b229f09ad05f643f634fd32299
akash-dew <40857953+akash-dew@users.noreply.github.com>
2021-07-19 14:14:34 +0530

    done changes
    done changes

--- apps/questionpaper/src/app/app.module.ts
+++ apps/questionpaper/src/app/app.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {QuestionPaperModule} from '../../../../libs/question-paper/src/lib/question-paper.module'
+import {QuestionPaperModule} from '../../../../libs/question-paper/src/lib/question-paper.module';
+import { ToolbarComponent } from './components/toolbar/toolbar.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ToolbarComponent],
   imports: [BrowserModule,QuestionPaperModule],
   providers: [],
   bootstrap: [AppComponent],
+  exports: [
+    ToolbarComponent
+  ],
 })
 export class AppModule {}
